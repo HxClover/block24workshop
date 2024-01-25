5659cb5067126bdecd9281c4e2485e74a29fdaed
hxclover <rbecca.lam@gmail.com>
2024-01-25 15:08:37 -0800

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,43 @@
-import { puppyList } from './data.js';
 import { useState } from 'react';
+import { puppyList } from './data.js';
+import './App.css';
 import './index.css';
 
 function App() {
 	const [puppies, setPuppies] = useState(puppyList);
+	const [featPupId, setFeatPupId] = useState(null);
 
-	console.log('puppies', puppyList);
+	function handleClick() {
+		// some logic here
+	}
+	const featuredPup = puppies.find((pup) => pup.id === featPupId);
+	console.log(featuredPup);
+
 	return (
 		<div className='App'>
+			{featPupId && (
+				<div>
+					<h2>{featuredPup.name}</h2>
+					<ul>
+						<li>Age: {featuredPup.age}</li>
+						<li>Email: {featuredPup.email}</li>
+					</ul>
+				</div>
+			)}
+			{featPupId !== null && <p>{featPupId}</p>}
 			{puppies.map((puppy) => {
-				return <p>{puppy.name}</p>;
+				return (
+					<p
+						onClick={() => {
+							setFeatPupId(puppy.id);
+						}}
+						key={puppy.id}
+					>
+						{puppy.name}
+					</p>
+				);
 			})}
 		</div>
 	);
 }
-
 export default App;
